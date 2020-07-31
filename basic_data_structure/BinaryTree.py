49480b43772170f675914180f0c88a1002134ace
esterlan <esteryukil@outlook.com>
2020-07-31 09:25:16 +0800

    7/31

--- basic_data_structure/BinaryTree.py
+++ basic_data_structure/BinaryTree.py
@@ -2,15 +2,103 @@
 # define the node of the tree
 class BiTreeNode:
     def __init__(self, val):
-        self._val = val
-        self._left =None
-        self._right = None
+        self.val = val
+        self.left =None
+        self.right = None
 
 
 # 3种遍历方法
 
-def preoderTrav(root: BiTreeNode):
-    if root is None:
-        return
-    else:
-    
+def preorderTrav(subtree: BiTreeNode):
+    ''' 先序(根)遍历'''
+    if subtree is not None:
+        print(subtree.val)
+        preorderTrav(subtree.left)
+        preorderTrav(subtree.right)
+
+def inorderTrav(subtree: BiTreeNode):
+    '''中(根)序遍历'''
+    if subtree is not None:
+        inorderTrav(subtree.left)
+        print(subtree.val)
+        inorderTrav(subtree.right)
+
+def postorderTrav(subtree: BiTreeNode):
+    '''后(根)序遍历'''
+    if subtree is not None:
+        inorderTrav(subtree.left)
+        inorderTrav(subtree.right)
+        print(subtree.val)
+
+# BFS bradth-First serach,宽度优先遍历
+# 一层一层遍历，使用queue
+def bradthFirstTrav(bintree):
+    from queue import Queue
+    q = Queue()
+    q.put(bintree)
+    while not q.empty():
+        node = q.get()
+        print(node.val)
+        if node.left is not None:
+            q.put(node.left)
+        if node.right is not None:
+            q.put(node.right)
+
+# 表达式树的节点类
+class _ExpTreeNode:
+    __slots__ = ('element', 'left', 'right')
+
+    def __init__(self,data):
+        self.element = data
+        self.left = None
+        self.right = None
+
+    def __repr__(self):
+        # 将实例转换为字符串时定义
+        # 注意与__str__区分
+        return '<_ExpTreeNode:{} {} {}>'.format(self.element, self.left,self.right)
+
+from queue import Queue
+class ExpressionTree:
+    '''
+        表达式树: 操作符存储在内节点操作数存储在叶子节点的二叉树。(符号树真难打出来)
+        *
+       / \
+      +   -
+     / \  / \
+     9  3 8   4
+    (9+3) * (8-4)
+
+    Expression Tree Abstract Data Type，可以实现二元操作符
+    ExpressionTree(expStr): user string as constructor param
+    evaluate(varDict): evaluates the expression and returns the numeric result
+    toString(): constructs and retutns a string represention of the expression
+
+    Usage:
+        vars = {'a': 5, 'b': 12}
+        expTree = ExpressionTree("(a/(b-3))")
+        print('The result = ', expTree.evaluate(vars))
+    '''
+
+    def __init__(self, expStr):
+        self._expTree = None
+        self._buildTree(expStr)
+
+
+
+     
+    def evaluate(self, varDict):
+        pass
+
+
+root = BiTreeNode(5)
+left_tRoot = BiTreeNode(3)
+right_tRoot = BiTreeNode(7)
+root.left = left_tRoot
+root.right = right_tRoot
+left_tRoot.left = BiTreeNode(2)
+left_tRoot.right = BiTreeNode(4)
+right_tRoot.left = BiTreeNode(6)
+right_tRoot.right = BiTreeNode(8)
+
+bradthFirstTrav(root)
--- basic_data_structure/Solution.py
+++ basic_data_structure/Solution.py
@@ -20,19 +20,123 @@ class Solution:
                 hi -= 1
         return rotateArray[lo]
 
-class Solution1:
-    def Fibonacci(self, n):
+# class Solution1:
+#     def Fibonacci(self, n):
+#         # write code here
+#         if n == 0:
+#             return 0
+#         elif n == 1:
+#             return 1
+#         else:
+#             return self.Fibonacci(n-1) + self.Fibonacci(n-2)
+
+
+# -*- coding:utf-8 -*-
+class Solution2:
+    def reOrderArray(self, array):
         # write code here
-        if n == 0:
-            return 0
-        elif n == 1:
-            return 1
+        if len(array) <= 1:
+            return array
+        mid = len(array) // 2
+        # lo = 0
+        hi = len(array) - 1
+        lo = mid
+        while lo>=0:
+            if array[lo] % 2 == 1:
+                # 前半部分，遇到奇数后移一位
+                lo -= 1
+            elif array[hi] % 2 == 0:
+                # 后半部分， 遇到偶数向前移一位
+                hi -= 1
+            else:
+                # 若有一不满足，则交换位置
+                array[lo], array[hi] = array[hi], array[lo]
+
+
+class Solution3:
+    def IsPopOrder(self, pushV, popV):
+        # write code here
+        if len(pushV) != len(popV):
+            # 长度不等
+            return False
+
+        temp = []
+        popV = popV[::-1]
+        for i in range(len(pushV)):
+            temp.append(pushV[i])
+            while temp[-1] == popV[-1] and len(temp) != 0 and len(popV) != 0:
+                # 若压入元素为弹出序列的首个元素，则将该元素弹出，并且popV的第一个元素也弹出
+                temp.pop()
+                popV.pop()
+        return len(popV) == 0
+
+
+# -*- coding:utf-8 -*-
+class Solution4:
+    def verifySquenceOfBST(self, sequence):
+        # write code here
+        if len(sequence) == 0:
+            return False
+        elif len(sequence) == 1 or len(sequence) == 2:
+            # 序列长度为1或者2，必为BST的后续遍历结果
+            return True
+        elif len(sequence) == 3:
+            # 考虑lambda表达式代替
+            if sequence[0] < sequence[-1] and sequence[1] > sequence[-1]:
+                return False
+            else:
+                return True
         else:
-            return self.Fibonacci(n-1) + self.Fibonacci(n-2)
+            root = sequence.pop()
+            i = -1
+            right_tree = []
+            while i >= len(sequence) * (-1) and sequence[i] > root:
+                right_tree.append(sequence.pop())
+                i -= 1
+            left_tree = sequence
+            return self.verifySquenceOfBST(right_tree) and self.verifySquenceOfBST(left_tree)
+
+class Solution5:
+    def NumberOf1Between1AndN(self, n)->int:
+        if n < 2**63-1:
+            l = list(range(1, n+1))
+            power = len(str(n))
+            res = sum([i%10==1 for i in l])
+            for p in range(1, power):
+                res += sum([i//(10**p)==1 for i in l])
+            return res
+
+class Solution6:
+    def PrintMinNumber(self, numbers):
+        length = len(numbers)
+        str_num = [str(i) for i in numbers]
+        min_num = int(''.join(str_num))
+        for j in range(length):
+            a = str_num[j]
+            new_str = [i for i in str_num if i != str_num[j]]
+            cur = a + new_str
+            cur = int(cur)
+            if cur < min_num:
+                min_num = cur
+        return min_num
 
 s = Solution()
 a = s.minNumberInRotateArray([6501,6828,6963,7036,7422,7674,8146,8468,8704,8717,9170,9359,9719,9895,9896,9913,9962,154,293,334,492,1323,1479,1539,1727,1870,1943,2383,2392,2996,3282,3812,3903,4465,4605,4665,4772,4828,5142,5437,5448,5668,5706,5725,6300,6335])
 print(a)
-s1 = Solution1()
-b = s1.Fibonacci(3)
-print(b)
+# s1 = Solution1()
+# b = s1.Fibonacci(3)
+s2 = Solution2()
+c = [1,2,3,4,5,6,7]
+s2.reOrderArray(c)
+s3 = Solution3()
+# s3.IsPopOrder([1,2,3,4,5],[4,5,3,2,1])
+s4 = Solution4()
+print(s4.verifySquenceOfBST([4,3,6,5,50,100,7]))
+
+s5 =Solution5()
+print(s5.NumberOf1Between1AndN(10000))
+
+s6 = Solution6()
+print(s6.PrintMinNumber([3,5,1,2,4]))
+
+#print(c)
